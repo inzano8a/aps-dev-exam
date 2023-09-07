d3c9082da26013ed901efc0874a9a82880584ef6
Juan José Ochoa <jj8a@hotmail.com>
2023-09-07 16:19:47 -0600

    Init

---
+++ src/main/java/com/activiti/extension/bean/CustomProcessEngineConfiguration.java
@@ -0,0 +1,8 @@
+package com.activiti.extension.bean;
+
+import com.activiti.api.engine.ProcessEngineConfigurationConfigurer;
+
+public class CustomProcessEngineConfiguration implements ProcessEngineConfigurationConfigurer
+{
+  
+}
